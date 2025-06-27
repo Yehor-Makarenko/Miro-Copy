a8c164d345d63edef8aab4e6118466ac7eefb260
Yehor Makarenko <mak.yegor@gmail.com>
2025-06-27 18:09:36 +0300

    Configure browser router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,26 +2,33 @@ import { createBrowserRouter, redirect } from "react-router";
 import App from "./app";
 import { ROUTES } from "@/shared/model/routes";
 
+const PAGES = {
+  BOARDS: "@/features/boards-list/board-list.page",
+  BOARD: "@/features/board/board.page",
+  LOGIN: "@/features/auth/login.page",
+  REGISTER: "@/features/auth/register.page",
+} as const;
+
 export const router = createBrowserRouter([
   {
     element: <App />,
     children: [
-      // {
-      //   path: ROUTES.BOARDS,
-      //   lazy: () => import("@/features/"),
-      // },
-      // {
-      //   path: ROUTES.BOARD,
-      //   lazy: () => import("@/features/"),
-      // },
-      // {
-      //   path: ROUTES.LOGIN,
-      //   lazy: () => import("@/features/"),
-      // },
-      // {
-      //   path: ROUTES.REGISTER,
-      //   lazy: () => import("@/features/"),
-      // },
+      {
+        path: ROUTES.BOARDS,
+        lazy: () => getLazyComponent(PAGES.BOARDS),
+      },
+      {
+        path: ROUTES.BOARD,
+        lazy: () => getLazyComponent(PAGES.BOARD),
+      },
+      {
+        path: ROUTES.LOGIN,
+        lazy: () => getLazyComponent(PAGES.LOGIN),
+      },
+      {
+        path: ROUTES.REGISTER,
+        lazy: () => getLazyComponent(PAGES.REGISTER),
+      },
       {
         path: ROUTES.HOME,
         loader: () => redirect(ROUTES.BOARDS),
@@ -29,3 +36,10 @@ export const router = createBrowserRouter([
     ],
   },
 ]);
+
+async function getLazyComponent<T extends keyof typeof PAGES>(
+  path: (typeof PAGES)[T],
+) {
+  const module = (await import(path)) as { default: React.ComponentType };
+  return { Component: module.default };
+}
