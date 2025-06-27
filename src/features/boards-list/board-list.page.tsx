0e85943f393b682d3296ee0333d7c24c9bd25ba5
Yehor Makarenko <mak.yegor@gmail.com>
2025-06-27 18:38:55 +0300

    Added .env file

--- src/features/boards-list/board-list.page.tsx
+++ src/features/boards-list/board-list.page.tsx
@@ -1,7 +1,9 @@
+import { CONFIG } from "@/shared/model/config";
 import { ROUTES, typedHref } from "@/shared/model/routes";
 import { Link } from "react-router";
 
 export default function BoardList() {
+  console.log(CONFIG.API_BASE_URL);
   return (
     <div>
       <Link to={typedHref(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
