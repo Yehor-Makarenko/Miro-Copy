0483b30696ca3f139f4725e83e5fe8d2614e716b
Yehor Makarenko <mak.yegor@gmail.com>
2025-06-28 23:23:03 +0300

    Install taiwindcss

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-gray-500">
       <AppHeader />
       <Outlet />
-    </>
+    </div>
   );
 }
