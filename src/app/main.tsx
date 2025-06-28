b1e6d9036a1977665bf97cb811644e614fc90937
Yehor Makarenko <mak.yegor@gmail.com>
2025-06-28 20:33:12 +0300

    Added mocks

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,17 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
+
+  const { worker } = await import("@/shared/api/mocks/browser");
+  return worker.start();
+}
+
+await enableMocking();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
---
+++ src/shared/api/mocks/handlers/index.ts
@@ -0,0 +1,50 @@
+import { HttpResponse } from "msw";
+import type { ApiSchemas } from "../../schema";
+import { http } from "../http";
+
+const boards: ApiSchemas["Board"][] = [
+  {
+    id: "Board1",
+    name: "Marketing",
+  },
+  {
+    id: "Board2",
+    name: "Product",
+  },
+  {
+    id: "Board3",
+    name: "English  ",
+  },
+];
+
+export const handlers = [
+  http.get("/boards", () => {
+    return HttpResponse.json(boards);
+  }),
+  http.post("/boards", async (query) => {
+    const data = await query.request.json();
+    const board: ApiSchemas["Board"] = {
+      id: crypto.randomUUID(),
+      name: data.name,
+    };
+
+    boards.push(board);
+    return HttpResponse.json(board);
+  }),
+  http.delete("/boards/{boardId}", (query) => {
+    const { boardId } = query.params;
+    const index = boards.findIndex((b) => b.id === boardId);
+    if (index === -1) {
+      return new HttpResponse(
+        {
+          message: "Board not found",
+          code: "NOT_FOUND",
+        },
+        { status: 404 },
+      );
+    }
+    boards.splice(index, 1);
+
+    return new HttpResponse(null, { status: 204 });
+  }),
+];
