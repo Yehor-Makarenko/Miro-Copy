27c7fde81349bcca62c281fc4a990ff8eafef0e9
Yehor Makarenko <mak.yegor@gmail.com>
2025-07-13 20:38:18 +0300

    Fix useToggleFavoriteBoard

--- src/features/boards-list/use-toggle-favorite-board.tsx
+++ src/features/boards-list/use-toggle-favorite-board.tsx
@@ -1,72 +1,45 @@
 import { rqClient } from "@/shared/api/instance";
 import { useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { startTransition, useOptimistic } from "react";
 
 export default function useToggleFavoriteBoard() {
   const queryClient = useQueryClient();
 
-  const [toggledBoards, setToggledBoards] = useState<Record<string, boolean>>(
-    {},
-  );
-
-  // const toggleBoardFavorite = (boardId: string) => {
-  //   queryClient.setQueriesData(
-  //     rqClient.queryOptions("get", "/boards"),
-  //     (
-  //       oldData: InfiniteData<ApiSchemas["BoardsList"]>,
-  //     ): InfiniteData<ApiSchemas["BoardsList"]> => {
-  //       if (!oldData) {
-  //         return oldData;
-  //       }
-
-  //       return {
-  //         ...oldData,
-  //         pages: oldData.pages.map((page) => ({
-  //           ...page,
-  //           list: page.list.map((board) => {
-  //             return board.id === boardId
-  //               ? { ...board, isFavorite: !board.isFavorite }
-  //               : board;
-  //           }),
-  //         })),
-  //       };
-  //     },
-  //   );
-  // };
+  const [toggledBoards, setToggledBoards] = useOptimistic<
+    Record<string, boolean>
+  >({});
 
   const toggleFavoriteMutation = rqClient.useMutation(
     "patch",
     "/boards/{boardId}/favorite",
     {
-      onMutate: ({ params, body }) => {
+      onMutate: () => {
         queryClient.cancelQueries(rqClient.queryOptions("get", "/boards"));
-
-        setToggledBoards((toggledBoards) => ({
-          ...toggledBoards,
-          [params.path.boardId]: body.isFavorite,
-        }));
       },
-      onSettled: async (_, __, { params }) => {
+      onSettled: async () => {
         await queryClient.invalidateQueries(
           rqClient.queryOptions("get", "/boards"),
         );
-        setToggledBoards((toggledBoards) => {
-          const newToggledBoards = { ...toggledBoards };
-          delete newToggledBoards[params.path.boardId];
-          return newToggledBoards;
-        });
       },
     },
   );
 
   return {
-    toggleFavorite: (boardId: string, isFavorite: boolean) =>
-      toggleFavoriteMutation.mutate({
-        params: { path: { boardId } },
-        body: {
-          isFavorite,
-        },
-      }),
+    toggleFavorite: (boardId: string, isFavorite: boolean) => {
+      startTransition(async () => {
+        setToggledBoards((toggledBoards) => ({
+          ...toggledBoards,
+          [boardId]: isFavorite,
+        }));
+
+        await toggleFavoriteMutation.mutateAsync({
+          params: { path: { boardId } },
+          body: {
+            isFavorite,
+          },
+        });
+      });
+    },
     getIsPending: (boardId: string) =>
       toggleFavoriteMutation.isPending && boardId in toggledBoards,
     getOptimisticFavorite: (board: { id: string; isFavorite: boolean }) =>
